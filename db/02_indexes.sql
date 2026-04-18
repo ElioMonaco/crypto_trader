ec317dc8c2027ee1bd5ebeed7d798cd4c75c45ec
ElioMonaco <elio.work@proton.me>
2026-04-18 12:54:07 +0200

    cleanup

--- db/02_indexes.sql
+++ db/02_indexes.sql
@@ -1,9 +1,10 @@
-CREATE UNIQUE INDEX idx_transaction_id ON market_events(transaction_id);
-CREATE INDEX idx_instrument_name ON market_events(instrument_name);
-CREATE INDEX idx_exchange_timestamp ON market_events(exchange_timestamp);
+-- =============================================================================
+-- 0100: INDEXES
+-- =============================================================================
 
-CREATE UNIQUE INDEX idx_transaction_id ON raw_market_events(transaction_id);
-CREATE INDEX idx_insert_timestamp ON raw_market_events(insert_timestamp);
+-- -----------------------------------------------------------------------------
+-- 0110: market_candles
+-- -----------------------------------------------------------------------------
 
 CREATE UNIQUE INDEX idx_transaction_id ON market_candles(transaction_id);
 CREATE INDEX idx_instrument_name ON market_candles(instrument_name);
--- db/06_cron.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS pg_cron;
-
-SELECT cron.schedule(
-  'recluster-market_events',
-  '0 3 * * * SUN', 
-  'CLUSTER market_events USING idx_exchange_timestamp;'
-);
